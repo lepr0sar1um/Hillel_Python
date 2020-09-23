098de46ee3e0005a6e35b9850bf34baa0049ca3f
Ivan Yeremenko <bw0711@gmail.com>
2020-09-23 21:12:41 +0300

    class work

---
+++ ClassWork/23.09.2020/classWork.py
@@ -0,0 +1,30 @@
+###
+# ternary operators
+###
+
+# x = int(input("Enter positive value: "))
+# # ternary operator in Python looks like that:
+# # y = 3 if x > 0 else -1
+# y = 7 if x % 7 == 0 else 0
+# print(y)
+
+###
+# string formatting
+###
+
+# var = 12345
+# # you need to use "f" char for string formatting
+# # see example below
+# some_string = f"some string = {var}"
+# print(some_string)
+# extra string methods:
+# https://pythonworld.ru/tipy-dannyx-v-python/stroki-funkcii-i-metody-strok.html
+
+###
+# cycle for
+# https://pythonworld.ru/osnovy/cikly-for-i-while-operatory-break-i-continue-volshebnoe-slovo-else.html
+###
+
+some_str = "qwertyuio"
+for value in some_str:
+    print(value * 20)
