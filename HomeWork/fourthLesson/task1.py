594bc60a8d0f82139af5f5381be65699eadd4eb0
Ivan Yeremenko <bw0711@gmail.com>
2020-09-30 10:43:51 +0300

    fix

--- HomeWork/fourthLesson/task1.py
+++ HomeWork/fourthLesson/task1.py
@@ -3,7 +3,7 @@ import random
 my_list = random.sample(range(0, 100), 10)
 my_list.sort(reverse=True)
 if my_list[0] < 100:
-    print("All list items are greater than 100")
+    print("All list items are less than 100")
 else:
     for value in my_list:
         if value > 100:
