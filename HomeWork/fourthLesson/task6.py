dc9b6fcdc9864e62f6555683e5e541af62d109f5
Ivan Yeremenko <bw0711@gmail.com>
2020-10-03 13:46:15 +0300

    cw

--- HomeWork/fourthLesson/task6.py
+++ HomeWork/fourthLesson/task6.py
@@ -1,4 +1,6 @@
 # task 6
+import random
+
 list_len = random.randint(0, 10)
 my_list_1 = random.sample(range(0, 200), list_len)
 my_list_2 = random.sample(range(0, 200), list_len)
@@ -7,4 +9,4 @@ print("Generated list of values: ", my_list_2, "\nIts len is: ", len(my_list_2))
 my_indexes = [x for x in range(list_len)]
 print(my_indexes)
 for index in my_indexes:
-    print((my_list_1[index],  my_list_2[index]))
+    print((my_list_1[index], my_list_2[index]))
