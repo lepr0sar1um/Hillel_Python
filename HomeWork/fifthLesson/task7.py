74f24da91e4eac3381a0ca46b06fc365e739ed1d
Ivan Yeremenko <bw0711@gmail.com>
2020-10-04 18:31:05 +0300

    home work

--- HomeWork/fifthLesson/task7.py
+++ HomeWork/fifthLesson/task7.py
@@ -3,6 +3,8 @@ import string
 
 letters = string.ascii_lowercase
 random_str = ''.join(random.choice(letters) for i in range(random.randint(1, 33)))
-my_list = [random_str[i:i + 2] + '_' if len(random_str[i:i + 2]) < 2
-           else random_str[i:i + 2] for i in range(0, len(random_str), 2)]
+random_str = random_str + '_' if len(random_str) % 2 else random_str
+my_list = [random_str[i:i + 2] for i in range(0, len(random_str), 2)]
+
 print(my_list)
+
