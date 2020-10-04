51b5364549a115067926de7c98e6c4dd5c42394b
Ivan Yeremenko <bw0711@gmail.com>
2020-10-05 01:11:38 +0300

    home work

--- HomeWork/fifthLesson/task10.py
+++ HomeWork/fifthLesson/task10.py
@@ -4,3 +4,5 @@ list_len = random.randint(5, 15)
 my_list = random.sample(range(15), list_len)
 list_of_elements = [my_list[i] for i in range(1, list_len - 1)
                     if my_list[i] > (my_list[i - 1] + my_list[i + 1])]
+print(list_of_elements)
+print(len(list_of_elements))
