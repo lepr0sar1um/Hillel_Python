f97ff3e5b44296871d723fb723f54566b350776e
Ivan Yeremenko <bw0711@gmail.com>
2020-10-09 17:09:24 +0300

    home work

--- HomeWork/seventhLesson/backyard.py
+++ HomeWork/seventhLesson/backyard.py
@@ -1,4 +1,7 @@
 import random
+import string
+
+letters = string.ascii_lowercase
 
 
 def random_list(size, min_range, max_range):
@@ -9,5 +12,35 @@ def random_tuple():
     return tuple(random.sample(range(-100, 100), 2))
 
 
+def random_dict():
+    return {key: random.randint(1, 100) for key in
+            list(''.join(random.choice(letters) for _ in range(random.randint(1, 3))))}
+
+
+# Return a list of the keys that are in both dictionaries
+def list_of_similar_keys(dict_a, dict_b):
+    return [key for key in dict_a if key in dict_b]
+
+
+# Return a list of the keys that are in the dict_a but not in the dict_b dictionary
+def list_of_different_keys(dict_a, dict_b):
+    return [key for key in dict_a if key not in dict_b]
+
+
+# Return a new dictionary of {key: value} pairs, for keys that are in the dict_a,
+# but not in the dict_b dictionary
+def new_dict_of_key_and_value(dict_a, dict_b):
+    return {key: value for (key, value) in dict_a.items() if key not in dict_b}
+
+
+# Combine these two dictionaries into a new dictionary according to the rule:
+# if the key is only in one of the two dictionaries, place a {key: value} pair,
+# if the key is in two dictionaries, put the pair {key: [value_from_dict_a, value_from_dict_b]}
+def joined_dict(dict_a, dict_b):
+    return {key: [dict_a.get(key), dict_b.get(key)] if key in dict_a.keys() & dict_b.keys() else dict_a.get(
+        key) or dict_b.get(key) for key in
+            dict_a.keys() | dict_b.keys()}
+
+
 def my_print(value):
-    return print("***", value, "***")
+    return print("*** ", value, " ***")
--- HomeWork/seventhLesson/homeWork.py
+++ HomeWork/seventhLesson/homeWork.py
@@ -6,3 +6,18 @@ make.my_print(my_list)
 my_dict = {key: make.random_tuple() for key in ('a', 'b', 'c')}
 make.my_print(my_dict)
 
+my_dict_1 = make.random_dict()
+my_dict_2 = make.random_dict()
+
+var1 = make.list_of_similar_keys(my_dict_1, my_dict_2)
+make.my_print(var1)
+
+var2 = make.list_of_different_keys(my_dict_1, my_dict_2)
+make.my_print(var2)
+
+var3 = make.new_dict_of_key_and_value(my_dict_1, my_dict_2)
+make.my_print(var3)
+
+var4 = make.joined_dict(my_dict_1, my_dict_2)
+make.my_print(var4)
+
