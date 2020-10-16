eaf378dc93b56a1dd72cd8de7dd8078cd4f9793c
Ivan Yeremenko <bw0711@gmail.com>
2020-10-16 14:34:48 +0300

    home work

--- HomeWork/lesson9/functions.py
+++ HomeWork/lesson9/functions.py
@@ -1,5 +1,5 @@
-from datetime import datetime
 import re
+from datetime import datetime
 
 
 def read_file(path: str) -> list:
@@ -7,7 +7,7 @@ def read_file(path: str) -> list:
     return [line.replace("\n", "") for line in file]
 
 
-def find_date(authors: list) -> list:
+def find_dates(authors: list) -> list:
     dates = []
     for line in authors:
         if "birthday" in line or "death" in line:
@@ -16,9 +16,25 @@ def find_date(authors: list) -> list:
 
 
 def convert_to_date(date_string: str) -> str:
-    return str(datetime.strptime(re.sub(r'(\d+)[a-z]+', r'\1', date_string),  '%d %B %Y').strftime('%d/%m/%Y'))
+    return str(datetime.strptime(re.sub(r'(\d+)[a-z]+', r'\1', date_string), '%d %B %Y').strftime('%d/%m/%Y'))
 
 
 def create_dict(dates: list) -> list:
     return [{"name": line.split("-")[1].split("'")[0].strip(), "date": convert_to_date(line.split("-")[0].strip())} for
             line in dates]
+
+
+def join_dates(dates: list) -> dict:
+    b_date_list = [line for line in dates if "birthday" in line]
+    d_date_list = [line for line in dates if "death" in line]
+
+    b_date_dict = {line.split("-")[1].split("'")[0].strip(): convert_to_date(line.split("-")[0].strip())
+                   for line in b_date_list}
+
+    d_date_dict = {line.split("-")[1].split("'")[0].strip(): convert_to_date(line.split("-")[0].strip())
+                   for line in d_date_list}
+
+    join_date = [{"name": key, "b_date": b_date_dict.get(key), "d_date": d_date_dict.get(key)} for key in
+                 b_date_dict.keys() | d_date_dict.keys()]
+
+    print(join_date)
--- HomeWork/lesson9/homeWork.py
+++ HomeWork/lesson9/homeWork.py
@@ -3,8 +3,5 @@ import Hillel_Python.HomeWork.lesson9.functions as f
 path = "/Users/ivanyeremenko/PycharmProjects/Hillel/Hillel_Python/HomeWork/authors.txt"
 
 authors = f.read_file(path)
-dates = f.find_date(authors)
-# print(authors)
-# print("\n".join(dates))
-
-print(f.create_dict(dates))
+dates = f.find_dates(authors)
+full_date = f.join_dates(dates)
